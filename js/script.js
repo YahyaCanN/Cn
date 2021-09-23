1bfa5ee803f8be1dc1e569af6da48bb2ebdcc1a1
YahyaCanN <89393704+YahyaCanN@users.noreply.github.com>
2021-09-23 10:41:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,83 @@
+window.sr = new ScrollReveal()
+
+sr.reveal(".text_upper h1", {
+    delay: 1000,
+    origin: "left",
+    distance: "70px"
+});
+
+sr.reveal(".text_upper p", {
+    delay: 1200,
+    distance: "70px"
+});
+
+sr.reveal(".content .col:nth-child(1) p", {
+    delay: 1000,
+    origin: "top",
+    distance: "70px"
+});
+
+sr.reveal(".row font", {
+    delay: 1200,
+    origin: "top",
+    distance: "70px"
+});
+
+sr.reveal(".content .row:nth-child(3)", {
+    delay: 1400,
+    origin: "top",
+    distance: "70px"
+});
+
+sr.reveal(".text_right", {
+    delay: 1500,
+    origin: "top",
+    interval: 400,
+    distance: "70px"
+});
+
+sr.reveal("#corporate h3", {
+    delay: 900,
+    origin: "left",
+    interval: 400,
+    distance: "70px"
+});
+
+sr.reveal("#section_2text", {
+    delay: 1500,
+    origin: "top",
+    distance: "70px"
+});
+
+sr.reveal("#section_2text h3", {
+    delay: 1750,
+    origin: "top",
+    distance: "70px"
+});
+
+sr.reveal("#section_2text p", {
+    delay: 1800,
+    origin: "top",
+    distance: "70px"
+});
+
+sr.reveal("#text_logo img", {
+    delay: 1800,
+    origin: "left",
+    distance: "70px"
+});
+
+sr.reveal("#arrow_icon", {
+    delay: 2000,
+    origin: "left",
+    distance: "100px"
+});
+
+sr.reveal("#section_2image", {
+    delay: 1500,
+    origin: "left",
+    distance: "100px"
+});
+
+
+
